b2503ac9cb290e106ecaa64b10bc2f4aac33ff03
Azryel <AzryelShroud@gmail.com>
2021-03-18 17:53:24 +0100

    bugfix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 //Set up mongoose connection
 var mongoose = require("mongoose");
 var mongoDB = "mongodb+srv://Azryel:mongo0atlas@cluster0.8u3xc.mongodb.net/local_library?retryWrites=true&w=majority";
-mongoose.connect(mongoDB, {useNewUrlParser: true , useUnifiedTopology = true});
+mongoose.connect(mongoDB, {useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
