6d06a35cd2269db8f49a575fcffe3bcf4fc90d04
Azryel <AzryelShroud@gmail.com>
2021-03-19 23:49:20 +0100

    update app for db configuration

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use(helmet());
 //database
 //Set up mongoose connection
 var mongoose = require("mongoose");
-var mongoDB = "mongodb+srv://Azryel:mongo0atlas@cluster0.8u3xc.mongodb.net/local_library?retryWrites=true&w=majority";
+var dev_db_url = "mongodb+srv://Azryel:mongo0atlas@cluster0.8u3xc.mongodb.net/local_library?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
