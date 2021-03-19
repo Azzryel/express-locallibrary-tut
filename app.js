b87fe30119dd55743db133a02fa9a00a92e4c1b6
Azryel <AzryelShroud@gmail.com>
2021-03-19 23:18:13 +0100

    update app use compression

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
 
+var compression = require('compression');
+
 var app = express();
 
 //database
@@ -26,6 +28,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression()); //Compress all routes
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
